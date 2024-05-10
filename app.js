86778bc2f1d62d67f8e4f25c8a2c80981c538c76
abidkk <dev.abidkk@gmail.com>
2024-05-10 22:57:14 +0500

    cards

--- app.js
+++ app.js
@@ -12,22 +12,3 @@ $(document).ready(function(){
 
 
 
-
-
-
-  <!-- Network Section -->
-  <div class="row network">
-
-      <div class="row network-card">
-          <div class="card-head">Network</div>
-          <div class="card-title"><span>Join Our Network. Get hired. Find clients. Build Your Business</span>
-              <span><img src="./public/icons/right-arrow.png" alt="icon" width="50" height="50"></span>
-          </div>
-          <div class="image-container">
-              <img src="./public/images/network1.webp" alt="image">
-          </div>
-      </div>
-
-
-
-  </div>
