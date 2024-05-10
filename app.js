586acc24dffe5832f59bf5c46b3b30933862e910
abidkk <dev.abidkk@gmail.com>
2024-05-10 22:56:46 +0500

    cards

--- app.js
+++ app.js
@@ -4,3 +4,30 @@ $(document).ready(function(){
       $(".menu-box").css({height:"300px"})
     });
   });
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Network Section -->
+  <div class="row network">
+
+      <div class="row network-card">
+          <div class="card-head">Network</div>
+          <div class="card-title"><span>Join Our Network. Get hired. Find clients. Build Your Business</span>
+              <span><img src="./public/icons/right-arrow.png" alt="icon" width="50" height="50"></span>
+          </div>
+          <div class="image-container">
+              <img src="./public/images/network1.webp" alt="image">
+          </div>
+      </div>
+
+
+
+  </div>
